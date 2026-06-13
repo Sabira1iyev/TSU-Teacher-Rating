56fb37ed1182497d93e8740fa21838a5ecf6eb96
Sabir Aliyev <s.aliyev2005@gmail.com>
2026-06-13 18:23:01 +0400

    editProfile/ fixed reviewCount problem in dashboard page

--- src/app/api/delete/route.ts
+++ src/app/api/delete/route.ts
@@ -18,6 +18,10 @@ export async function DELETE(req: NextRequest) {
 
     const db = await getDb();
 
+    const profIdResult = await db.request().input("userId", userId).query(`
+      SELECT ProfessorId FROM Reviews WHERE UserId = @userId`);
+    const profIds = profIdResult.recordset.map((row) => row.ProfessorId);
+
     await db.request().input("userId", userId).query(`
         DELETE FROM ReviewTags WHERE ReviewId IN (SELECT ReviewId FROM Reviews WHERE UserId = @userId);
         DELETE FROM ReviewInteractions WHERE ReviewId IN (SELECT ReviewId FROM Reviews WHERE UserId = @userId);
@@ -26,6 +30,15 @@ export async function DELETE(req: NextRequest) {
         DELETE FROM Reviews WHERE UserId = @userId;
         DELETE FROM Users WHERE UserId = @userId;
         `);
+
+    for (const profId of profIds) {
+      await db.request().input("ProfId", profId).query(`
+            UPDATE Professors
+            SET 
+            reviewCount = (SELECT COUNT(*) FROM Reviews WHERE ProfessorId = @ProfId),
+            AverageRating = ISNULL((SELECT AVG(CAST(OverallRating as FLOAT)) FROM Reviews WHERE ProfessorId = @ProfId), 0.00)
+            WHERE ProfessorId = @ProfId`);
+    }
 
     return NextResponse.json(
       {
