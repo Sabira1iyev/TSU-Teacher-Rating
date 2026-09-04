5a707c4a5786788c60aebb860feab2a9979da9ff
Sabir Aliyev <s.aliyev2005@gmail.com>
2026-09-04 23:31:12 +0400

    fix: handle registration for unverified existing accounts

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -40,23 +40,88 @@ export async function POST(req: NextRequest) {
     const existing = await db
       .request()
       .input("Email", email)
-      .query("SELECT UserId FROM Users WHERE Email = @Email");
-
-    if (existing.recordset.length > 0) {
-      return NextResponse.json(
-        { error: "This email is already registered." },
-        { status: 400 },
-      );
-    }
-
-    // create hash password
-    const passwordHash = await bcrypt.hash(password, 12);
+      .query("SELECT UserId, IsVerified FROM Users WHERE Email = @Email");
 
     const verificationCode = Math.floor(
       100000 + Math.random() * 900000,
     ).toString();
     const expiryDate = new Date();
     expiryDate.setMinutes(expiryDate.getMinutes() + 10);
+
+    const passwordHash = await bcrypt.hash(password, 12);
+
+    if (existing.recordset.length > 0) {
+      const isVerified = existing.recordset[0].IsVerified;
+      if (isVerified) {
+        return NextResponse.json(
+          {
+            message: "This email is already verified, please login!",
+          },
+          {
+            status: 400,
+          },
+        );
+      } else {
+        await db
+          .request()
+          .input("PasswordHash", passwordHash)
+          .input("VerificationCode", verificationCode)
+          .input("VerificationExpiry", expiryDate)
+          .input("Email", email)
+          .query(
+            `
+          Update Users
+          SET PasswordHash = @PasswordHash, VerificationCode = @VerificationCode, VerificationExpiry = @VerificationExpiry 
+          Where Email = @Email
+          `,
+          );
+        const transporter = nodemailer.createTransport({
+          service: "gmail",
+          auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+          },
+        });
+
+        const mailOptions = {
+          from: `"TSU Teacher Rating" <${process.env.EMAIL_USER}>`,
+          to: email,
+          subject: "Your Verification Code",
+          text: `Your verification code is: ${verificationCode}. It will expire in 10 minutes.`,
+          html: `
+        <div style="font-family: Arial, sans-serif; padding: 20px;">
+          <h2>Email Verification</h2>
+          <p>Thank you for registering. Your verification code is:</p>
+          <h1 style="color: #0060a9; letter-spacing: 5px;">${verificationCode}</h1>
+          <p>This code will expire in 10 minutes.</p>
+        </div>
+      `,
+        };
+
+        try {
+          await transporter.sendMail(mailOptions);
+        } catch (error) {
+          console.log("Failed to sende verification code", error);
+          return NextResponse.json(
+            {
+              message:
+                "Registration successfull but failed to send verification code!",
+            },
+            {
+              status: 200,
+            },
+          );
+        }
+        return NextResponse.json(
+          {
+            message: "Verification code resent successfully",
+          },
+          {
+            status: 200,
+          },
+        );
+      }
+    }
 
     const result = await db
       .request()
